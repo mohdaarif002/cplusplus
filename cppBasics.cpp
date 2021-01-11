79f5d47732b5e1a315bb33d765f93413910af914
Mohd Aarif <aarif75375@gmail.com>
2021-01-11 15:37:45 +0530

    Update cppBasics.cpp
    basics updated 1

--- cppBasics.cpp
+++ cppBasics.cpp
@@ -4,23 +4,42 @@ using namespace std;
 
 
 int main(){
-/*       data types
+
+            //data types
 char a;
 int b;
 float c;
-long double d;
+double d;
 bool g=true;
 
-cout << sizeof(d);
-*/
+cout << sizeof(d);   //gives size in Bytes
 
-// char name[100]="aarif";                it is a c lang way
-// cout<<"Hye this is my value "<<name[0]<<endl;
 
-string add;
-getline(cin,add);
-cout<<add<<endl;
-cout<<add.size()<<endl;
+ char name[6]="aarif"; // stores last element as \0
+ cout<<name<<endl;
+
+
+int arr[]={1,2,4,5};
+ int len=sizeof(arr)/sizeof(arr[0]);   //calculate length of any array
+ for(int i=0;i<len;i++){
+   cout << arr[i]<<endl;
+ }
+
+int i=0;
+while(i<11){
+        cout<<"i is under 11: "<<i<<endl;
+i++;
+}
+
+
+string s1;
+string s2="buddy";              // see other useful string functions
+getline(cin,s1);
+cout<<s1<<endl;
+cout<<s1.length()<<endl;
+s2=s2+s1;
+cout<<s2<<endl;
+
 
 
 
